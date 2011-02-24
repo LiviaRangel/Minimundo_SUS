003ed92bf8e747f80d4f2153d56b2bb5c41f9aee
LiviaRangel <rangel.livia@gmail.com>
2011-02-24 17:03:49 -0300

    Relatorio disponivel: Internacoes por hospital

--- Internacao.py
+++ Internacao.py
@@ -1,7 +1,20 @@
 class Internacao:
-    def __init__(self, codigo, data_inicio, data_fim, medico, paciente):
+
+    Internacoes = []
+    def __init__(self, codigo, data_inicio, data_fim, medico, paciente, hospital):
         self.codigo = codigo
         self.data_inicio = data_inicio
         self.data_fim = data_fim
         self.medico = medico
         self.paciente = paciente
+	self.hospital = hospital
+	self.gravarInternacao(self)
+
+    def gravarInternacao(self, internacao):
+        self.Internacoes.append(internacao)
+
+
+class FecharInternacao:
+    def __init__(self, codigo, data_fim):
+	pass
+
